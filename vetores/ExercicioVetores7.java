53aa05b4be43e73e2e8616e3011e4759afeccb03
Gabrielle Cristina <gabrielle.cristina1@gmail.com>
2022-08-18 17:37:53 -0300

    exc 7 vetores

---
+++ vetores/ExercicioVetores7.java
@@ -0,0 +1,40 @@
+
+package vetores;
+
+import java.util.Scanner;
+
+/**
+ *criar dois vetores com 10
+ * criar vetor c onde cada elemento é a subtração dos elementos de a e b.
+ */
+public class ExercicioVetores7 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in); 
+        int[] a = new int[3];
+        int[] b = new int[3];
+        int[] c = new int[3];
+        
+        for(int i=0;i<a.length;i++){
+            System.out.println("Digite os valores do índice A: ");
+        a[i]=s.nextInt();
+        }
+        for(int i=0;i<b.length;i++){
+            System.out.println("Agora digite os valores do índice B: ");
+        b[i]=s.nextInt();
+        c[i]=a[i]-b[i];
+        }
+        System.out.println("Os valores em A são: ");
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]+ " ");
+        }
+        System.out.println(" |Os valores em B são: ");
+        for(int i=0;i<b.length;i++){
+            System.out.println(b[i]+ " ");
+        }
+        System.out.println(" |Os valores em C são: ");
+        for(int i=0;i<a.length;i++){
+            System.out.println(c[i]+ " ");
+    }
+}
+}
