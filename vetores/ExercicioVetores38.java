b33cc586aae271c1568627981954e870ce8140f8
Gabrielle Cristina <gabrielle.cristina1@gmail.com>
2022-09-01 14:13:24 -0300

    Create ExercicioVetores38.java

---
+++ vetores/ExercicioVetores38.java
@@ -0,0 +1,18 @@
+
+package vetores;
+
+import java.util.Scanner;
+
+/**
+ *vetor a e b 10 int.programa q realize a interseção de a e b para vetor c
+ * interseção:tds os elementos que existem em A tbm existem em B.
+ * 
+ */
+public class ExercicioVetores38 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] a = new int[10];
+        int[] b= new int[a.length];
+    }
+}
