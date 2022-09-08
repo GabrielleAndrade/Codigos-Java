ca7427a7c397053c4b521d150fa12d87805d5691
Gabrielle Cristina <gabrielle.cristina1@gmail.com>
2022-09-08 15:26:02 -0300

    Update ExercicioVetores39.java

--- vetores/ExercicioVetores39.java
+++ vetores/ExercicioVetores39.java
@@ -12,30 +12,27 @@ public class ExercicioVetores39 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int[] a = new int[10];
+        int[] a = new int[5];
         int[] b = new int[a.length];
         int[] c = new int[a.length];
-
+        int contadorc=0;
         for (int i = 0; i < a.length; i++) {
             System.out.println("Digite o valor de A no indice: " + i);
             a[i] = s.nextInt();
-        }
-        for (int i = 0; i < b.length; i++) {
             System.out.println("Digite o valor de B no indice: " + i);
             b[i] = s.nextInt();
+
         }
-
-        for (int i = 0; i < a.length; i++) {
-
-            int result = 0;
-
-            for (int j = 0; j < b.length; j++) {
-                if (!(a[i] == b[j])) {
-                    result++;
+       for (int i = 0; i < a.length; i++) {
+           int contador=0;
+            for (int j = 0; j < a.length; j++) {
+                if (!(a[i]==b[j])){
+                    contador++;
                 }
             }
-            if (a.length == result) {
-                c[i] = a[i];
+            if(contador==b.length){
+                c[contadorc]=a[i];
+                contadorc++;
             }
         }
 
@@ -48,7 +45,7 @@ public class ExercicioVetores39 {
             System.out.print(b[i] + " ");
         }
         System.out.print("\nValores em C");
-        for (int i = 0; i <c.length; i++) {
+        for (int i = 0; i <contadorc; i++) {
             System.out.print(c[i] + " ");
         }
     }
